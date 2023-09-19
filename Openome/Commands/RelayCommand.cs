5221e3b7cb628ac8a83cbc02b8177ab623a7924a
Tiago Júnio Tegon Nascimento <tiago.tegon@gmail.com>
2023-09-19 20:47:16 -0300

    Add project files.

---
+++ Openome/Commands/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Openome.Commands
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter) => _execute(parameter!);
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
